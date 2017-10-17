83d574cbaaf56b83bee1a6c0d84e17670042b32f
AakashRaina <aakashraina9@gmail.com>
2017-10-17 12:28:53 +0530

    Filter screen done

--- app/components/Filter.js
+++ app/components/Filter.js
@@ -5,21 +5,21 @@ import {
   StyleSheet,
   Picker,
   PickerItem,
-  Button
+  TextInput
 } from "react-native";
 import Header from "./Header";
+import { Icon } from "react-native-elements";
 
 const years = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
 const genres = [];
-const rating = 0;
 
 export default class Filter extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedYear: 2017,
-      selectedGenre: "Action",
+      selectedYear: " ",
+      selectedGenre: " ",
       selectedRating: 0
     };
   }
@@ -27,6 +27,7 @@ export default class Filter extends React.Component {
   yearItems = [];
   genreItems = [];
 
+  // Get the genres from the api endpoint //
   async getGenres() {
     response = await fetch(
       "https://api.themoviedb.org/3/genre/movie/list?api_key=8b51b25335ed94c74571c812120a6c73"
@@ -35,10 +36,10 @@ export default class Filter extends React.Component {
     responseJson = await response.json();
     genres = responseJson.genres;
 
+    // To just get the genre names for genre picker items //
     for (var i = 0; i < genres.length; i++) {
       s = genres[i];
-      console.log(s.name);
-      // sLabel = s.name.toString();
+
       this.genreItems.push(
         <Picker.Item key={i} value={s.name} label={s.name} />
       );
@@ -48,6 +49,7 @@ export default class Filter extends React.Component {
   componentWillMount() {
     this.getGenres();
 
+    // To get the years for years picker item //
     for (var i = 0; i < years.length; i++) {
       s = years[i];
       sLabel = s.toString();
@@ -86,9 +88,22 @@ export default class Filter extends React.Component {
 
           <View style={styles.votesview}>
             <Text style={{ fontSize: 30, fontWeight: "bold" }}>Rating</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={"Only Numeric Values .."}
+              keyboardType="numeric"
+              onChangeText={text => this.setState({ selectedRating: text })}
+            />
           </View>
 
-          <View style={styles.button} />
+          <View style={styles.button}>
+            <Icon
+              raised
+              name="search"
+              type="font-awesome"
+              onPress={() => console.log("hello")}
+            />
+          </View>
         </View>
       </View>
     );
@@ -101,20 +116,27 @@ const styles = StyleSheet.create({
     backgroundColor: "white"
   },
   filterview: {
-    flex: 4 / 5,
-    justifyContent: "space-around"
+    flex: 4 / 5
   },
   yearview: {
-    backgroundColor: "red"
+    flex: 1 / 4,
+   
   },
   genreview: {
-    backgroundColor: "yellow"
+    flex: 1 / 4
   },
   votesview: {
-    backgroundColor: "green"
+    flex: 1 / 4
   },
   button: {
+    flex: 1 / 4,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    
+  },
+  input: {
+    marginTop: 10,
+    paddingTop: 1,
+    height: 25
   }
 });
